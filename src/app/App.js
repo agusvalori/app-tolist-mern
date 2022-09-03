2261d78b37fc77c165fb43faacada3f7d7ff38db
agusvalori <61027212+agusvalori@users.noreply.github.com>
2022-09-02 22:11:03 -0300

    Configurando express para aceptar reactjs

---
+++ src/app/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {Box,Typography} from '@mui/material';
+
+export const App = () => {
+  return (
+    <div>
+        <Box>
+            <Typography>Aca desde reactjs con materia ui</Typography>
+        </Box>
+        <Box>
+            
+        </Box>
+        
+    </div>
+    
+
+  )
+}
--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,12 @@
-alert('react goes her')
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { App } from './App';
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('app'));
+root.render(
+  <React.StrictMode>
+    <App/>
+  </React.StrictMode>
+);
